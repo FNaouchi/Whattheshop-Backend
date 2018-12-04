3a13a3cacd360ccdfab6157b72f32c8141c12cdf
HessahAlK <hessah.alkhudhair@gmail.com>
2018-12-04 10:19:30 +0300

    Deployment

--- wtshop/settings.py
+++ wtshop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+sb2j2pxmv^i(xvyt8wjc0l5pe*0c@$nqh3d7@u%&f)+2(mg=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['104.248.37.122']
 
 
 # Application definition
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'wtshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql_psycopg2'),
     }
 }
 
